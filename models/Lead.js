08d2f199e6a2a25481f4a06fc15249c19d56dd02
infinitystones207-netizen <infinitystones207@gmail.com>
2025-08-28 11:46:47 -0500

    Update Lead.js

--- models/Lead.js
+++ models/Lead.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const leadSchema = new mongoose.Schema({
+  business: { type: mongoose.Schema.Types.ObjectId, ref: "Business", required: true },
+  name: String,
+  phone: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Lead", leadSchema);
